767485b16cebae2e0bac17d916cf10fcc0ab6c05
Gaaaamma <ytching552022@gmail.com>
2023-02-15 00:35:36 +0800

    Add: #94 BinaryTreePostorderTraversal - use iterative way to solve

--- easy/pb0094.cpp
+++ easy/pb0094.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <unordered_set>
 using namespace std;
 
 /*
@@ -8,12 +9,14 @@ using namespace std;
 Attempt 1: 
 Use recursive way to solve
 
+Attempt 2:
+Use iterative way to solve
+
 Complexity: O(N) if only output the value
 
 Q: Given the root of a binary tree, 
    return the inorder traversal of its nodes' values.
 */     
-
 
 void output(vector<int> nums) {
     if (nums.empty()) {
@@ -47,6 +50,31 @@ public:
         return result;
     }
     
+    vector<int> inorderTraversalIter(TreeNode* root) {
+        if (root == nullptr) return vector<int>{};
+        vector<int> result;
+        stack<TreeNode*> treeStack;
+        unordered_set<TreeNode*> traversed;
+        treeStack.push(root);
+
+        while (!treeStack.empty()) {
+            TreeNode* traverse = treeStack.top();
+            if (traverse->left != nullptr && !traversed.count(traverse->left)) {
+                traverse = traverse->left;
+                treeStack.push(traverse);
+                continue;
+            }
+            result.push_back(traverse->val);
+            traversed.insert(traverse);
+            treeStack.pop();
+            if (traverse->right != nullptr && !traversed.count(traverse->right)) {
+                traverse = traverse->right;
+                treeStack.push(traverse);
+            }
+        }
+        return result;
+    }
+
     TreeNode* createBST(vector<int> nums) {
         TreeNode* head = new TreeNode(nums[0]);
         for (int i = 1; i < nums.size(); i++) {
@@ -74,7 +102,8 @@ int main(int argc, char* argv[]) {
     vector<vector<int>> input{{5,2,1,6,3,4,9,7,8,10}, {1}, {5,6,2,7,5,3,7,1,9,2}};
     Solution solution;
     for (int i = 0; i < input.size(); i++) {
-        cout << "Inorder:   ";
+        cout << "Inorder:\n";
         output(solution.inorderTraversal(solution.createBST(input[i])));
+        output(solution.inorderTraversalIter(solution.createBST(input[i])));
     }
 }
