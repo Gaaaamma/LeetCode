a462fe9f9b1cda9c5a9d6fa8a01f5e2544fb57f5
Gaaaamma <ytching552022@gmail.com>
2023-02-14 17:38:01 +0800

    Add: #145 BinaryTreePostorderTraversal - use recursive way to solve

---
+++ easy/pb0145.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+/*
+#145 Binary Tree Postorder Traversal
+Attempt 1: 
+Use recursive way to solve
+
+Complexity: O(N) if only output the value
+
+Q: Given the root of a binary tree, 
+   return the postorder traversal of its nodes' values.
+*/     
+
+
+void output(vector<int> nums) {
+    if (nums.empty()) {
+        cout << "[]\n";
+    } else {
+        cout << "[";
+        for (int i = 0; i < nums.size() - 1; i++) {
+            cout << nums[i] << ", " ;
+        }
+        cout << nums.back() << "]\n";
+    }
+}
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val) : val(val), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (root == nullptr) return vector<int>{};
+        vector<int> result;
+        vector<int> left = postorderTraversal(root->left);
+        vector<int> right = postorderTraversal(root->right);
+        result.insert(result.end(), left.begin(), left.end());
+        result.insert(result.end(), right.begin(), right.end());
+        result.push_back(root->val);
+        return result;
+    }
+    
+    TreeNode* createBST(vector<int> nums) {
+        TreeNode* head = new TreeNode(nums[0]);
+        for (int i = 1; i < nums.size(); i++) {
+            TreeNode* traverse = head;
+            TreeNode* tmp = new TreeNode(nums[i]);
+            while (traverse != nullptr) {
+                if (tmp->val >= traverse->val && traverse->right != nullptr) {
+                    traverse = traverse->right;
+                } else if (tmp->val >= traverse->val && traverse->right == nullptr) {
+                    traverse->right = tmp;
+                    break;
+                } else if (tmp->val < traverse->val && traverse->left != nullptr) {
+                    traverse = traverse->left;
+                } else {
+                    traverse->left = tmp;
+                    break;
+                }
+            }
+        }
+        return head;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    vector<vector<int>> input{{5,2,1,6,3,4,9,7,8,10}, {1}, {5,6,2,7,5,3,7,1,9,2}};
+    Solution solution;
+    for (int i = 0; i < input.size(); i++) {
+        cout << "Postorder:   ";
+        output(solution.postorderTraversal(solution.createBST(input[i])));
+    }
+}
