dcce03aaf8469414dc6448f455a686c21aedee1a
Gaaaamma <ytching552022@gmail.com>
2023-02-17 11:46:09 +0800

    Add: #226 InvertBinaryTree - use recursive way to solve

---
+++ easy/pb0226.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std;
+
+/*
+#226 Invert Binary Tree
+Attempt 1: 
+Use recursive way to solve
+
+Complexity: O(N) 
+
+Q: Given the root of a binary tree, 
+   invert the tree, and return its root.
+*/     
+
+const int null = INT_MAX;
+void output(vector<int> nums) {
+    if (nums.empty()) {
+        cout << "[]\n";
+    } else {
+        cout << "[";
+        for (int i = 0; i < nums.size() - 1; i++) {
+            cout << nums[i] << ", " ;
+        }
+        cout << nums.back() << "]\n";
+    }
+}
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val) : val(val), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) return nullptr;
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (root == nullptr) return vector<int>{};
+        vector<int> result;
+        vector<int> left = inorderTraversal(root->left);
+        vector<int> right = inorderTraversal(root->right);
+        result.insert(result.end(), left.begin(), left.end());
+        result.push_back(root->val);
+        result.insert(result.end(), right.begin(), right.end());
+        return result;
+    }
+    TreeNode* createBST(vector<int> nums) {
+        TreeNode* head = new TreeNode(nums[0]);
+        for (int i = 1; i < nums.size(); i++) {
+            TreeNode* traverse = head;
+            TreeNode* tmp = new TreeNode(nums[i]);
+            while (traverse != nullptr) {
+                if (tmp->val >= traverse->val && traverse->right != nullptr) {
+                    traverse = traverse->right;
+                } else if (tmp->val >= traverse->val && traverse->right == nullptr) {
+                    traverse->right = tmp;
+                    break;
+                } else if (tmp->val < traverse->val && traverse->left != nullptr) {
+                    traverse = traverse->left;
+                } else {
+                    traverse->left = tmp;
+                    break;
+                }
+            }
+        }
+        return head;
+    }
+    TreeNode* createBT(vector<int> nums) {
+        if (nums.empty() || nums[0] == null) return nullptr;
+        int index = 0;
+        TreeNode* root = new TreeNode(nums[0]);
+        queue<TreeNode*> treeQueue;
+        treeQueue.push(root);
+
+        while (!treeQueue.empty()) {
+            if (nums[index] == null) {
+                index++;
+                if (index >= nums.size())
+                    break;
+                continue;
+            }
+
+            TreeNode* traverse = treeQueue.front();
+            if (index * 2 + 1 < nums.size() && nums[index * 2 + 1] != null) {
+                traverse->left = new TreeNode(nums[index * 2 + 1]);
+                treeQueue.push(traverse->left);
+            }
+            if (index * 2 + 2 < nums.size() && nums[index * 2 + 2] != null) {
+                traverse->right = new TreeNode(nums[index * 2 + 2]);
+                treeQueue.push(traverse->right);
+            }
+            treeQueue.pop();
+            index++;
+        }
+        return root;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Solution solution;
+    vector<vector<int>> input{{4,2,7,1,3,6,9}, {2,1,3}, {1,null,2}, {1}, 
+    {0,1,null,3,null,null,null,7,null,null,null,null,null,null,null,15}, {}};
+
+    for (int i = 0; i < input.size(); i++) {
+        output(solution.inorderTraversal(solution.createBT(input[i])));
+        output(solution.inorderTraversal(solution.invertTree(solution.createBT(input[i]))));
+        cout << "\n";
+    }
+}
