06e168f1893d33b2dd1e6d92afeec77067d77962
Gaaaamma <ytching552022@gmail.com>
2023-02-18 10:22:02 +0800

    Add: #101 SymmetricTree - use recursive compare to solve

--- easy/pb0101.cpp
+++ easy/pb0101.cpp
@@ -7,7 +7,7 @@ using namespace std;
 /*
 #101 Symmetric Tree
 Attempt 1: 
-Invert root->right tree and you inorder to compare left and right side tree
+Use recursive compare to solve
 Complexity: O(N) 
 
 Q: Given the root of a binary tree, 
@@ -37,51 +37,18 @@ struct TreeNode {
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if (root == nullptr) return false;;
-        if (root->left == nullptr && root->right ==nullptr) return true;
-
-        root->right = invertTree(root->right);
-        vector<int> left = inorderTraversal(root->left);
-        vector<int> right = inorderTraversal(root->right);
-        if (left.size() != right.size()) return false;
-        for (int i = 0; i < left.size(); i++) {
-            if (left[i] != right[i])
-                return false;
-        }
+        if (root == nullptr) return false;
+        return compare(root->left, root->right);
+    }
+    bool compare(TreeNode* treeA, TreeNode* treeB) {
+        if (treeA == nullptr && treeB == nullptr) return true;
+        if (treeA != nullptr && treeB == nullptr) return false;
+        if (treeA == nullptr && treeB != nullptr) return false;
+        if (compare(treeA->left, treeB->right) == false) return false;
+        if (compare(treeA->right, treeB->left) == false) return false;
+        if (treeA->val != treeB->val) return false;
         return true;
     }
-    vector<int> inorderTraversal(TreeNode* root) {
-        if (root == nullptr) return vector<int>{null};
-        vector<int> result;
-        stack<TreeNode*> treeStack;
-        treeStack.push(root);
-        while (!treeStack.empty()) {
-            TreeNode* traverse = treeStack.top();
-            if (traverse != nullptr) {
-                treeStack.pop();
-                if (traverse->right != nullptr) treeStack.push(traverse->right);
-                else result.push_back(null);
-                treeStack.push(traverse);
-                treeStack.push(nullptr);
-                if (traverse->left != nullptr) treeStack.push(traverse->left);
-                else result.push_back(null);
-            } else {
-                treeStack.pop();
-                result.push_back(treeStack.top()->val);
-                treeStack.pop();
-            }
-        }
-        return result;
-    }
-    TreeNode* invertTree(TreeNode* root) {
-        if (root == nullptr) return nullptr;
-        TreeNode* left= invertTree(root->left);
-        TreeNode* right= invertTree(root->right);
-        root->left = right;
-        root->right = left;
-        return root;
-    }
-
     TreeNode* createBST(vector<int> nums) {
         TreeNode* head = new TreeNode(nums[0]);
         for (int i = 1; i < nums.size(); i++) {
