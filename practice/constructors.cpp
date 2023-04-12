3929b8ab1905621cf3331731da92092722633afc
Gaaaamma <ytching552022@gmail.com>
2023-04-12 15:25:56 +0800

    Add: #prac constructors - compare difference between 3 types of constructor

---
+++ practice/constructors.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Car {
+private:
+    int age;
+    string brand;
+public:
+    Car() : age(2000), brand("DEFAULT") {cout << "default\n";}
+    Car(int age, string brand) : age(age), brand(brand) {cout << "default value\n";}
+    Car(const Car& obj) : age(obj.age), brand(obj.brand) {cout << "copy\n";}
+    Car(Car&& obj) : age(move(obj.age)), brand(move(obj.brand)) {cout << "move\n";}
+
+    void showInfo() {cout << this->brand << "_" << this->age << "\n";}
+    void setAge(int age) {this->age = age;}
+    void setBrand(string brand) {this->brand = brand;}
+};
+int main(int argc, char* argv[]) {
+    Car carA(2020, "AAA");
+    Car carB = carA;
+    Car carC = move(carB);
+
+    cout << "carA: ";
+    carA.showInfo();
+    cout << "carB: copy from carA (had been moved to carC) ";
+    carB.showInfo();
+    cout << "carC: (get resource from carB) ";
+    carC.showInfo();
+}
